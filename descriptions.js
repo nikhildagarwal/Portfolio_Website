3d73dc1f5894b0694ee8ac4645e6c8c19e307114
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-02-17 10:42:49 -0500

    updates

--- descriptions.js
+++ descriptions.js
@@ -1,7 +1,12 @@
 (function (){
     [...document.querySelectorAll(".project-description")].forEach(button => {
         button.addEventListener("click",function(){
-            console.log("Hello");
+            //console.log(button.classList);
+            //console.log("jo");
+            //console.log(button.className);
+            document.querySelector(".active").classList.remove("active");
+            button.className += " active";
+            
         })
     })
 })();
