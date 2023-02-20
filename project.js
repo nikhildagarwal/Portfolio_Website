81596c8bd631760dd765b2e0f642abd21e6804f6
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-02-19 20:19:53 -0500

    add json

--- project.js
+++ project.js
@@ -1,4 +1,5 @@
 (function (){
+    const hi = JSON.parse("mode.JSON");
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
