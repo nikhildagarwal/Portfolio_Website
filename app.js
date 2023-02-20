46d4d6ee8597df1df0ee33f2017b9e1f0e1a7934
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-02-20 05:03:43 -0500

    still have to fix writing too json

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
+
 (function () {
-    /*[...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
-    });*/
+    
     var darkMode = "dark";
     var lightMode = "light";
     var request = new XMLHttpRequest();
     request.open("GET", "myMode.json", false);
     request.send(null)
     var my_JSON_object = JSON.parse(request.responseText);
-    var currState = my_JSON_object[0];
     
+    var currState = my_JSON_object.theme;
     if(currState==lightMode){
         document.body.classList.toggle("light-mode");
     }
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-        if(currState==lightMode){
-            
-        }else{
-            
+        const nextState = {
+            theme:''
         }
+        if(currState==lightMode){
+            nextState.theme = darkMode;
+        }else{
+            nextState.theme = lightMode;
+        }
+        
+        currState = nextState.theme;
+        //console.log(nextState);
+       
     })
 })();
